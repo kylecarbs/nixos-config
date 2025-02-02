005df8f3f0427ed568fadb31e229bbb5acf6d651
Kyle Carberry <kyle@carberry.com>
2025-02-02 14:01:14 -0500

    Fix chat script

--- hosts/home.nix
+++ hosts/home.nix
@@ -193,7 +193,7 @@ in
 
   # Add all of our binaries!
   home.file.".local/bin/ams".source = ../bin/ams;
-  home.file.".local/bin/chatgpt".source = ../bin/chatgpt;
+  home.file.".local/bin/chat".source = ../bin/chat;
   home.file.".local/bin/dmenu_emoji".source = ../bin/dmenu_emoji;
   home.file.".local/bin/git-hf".source = ../bin/git-hf;
   home.file.".local/bin/mypulls".source = ../bin/mypulls;
