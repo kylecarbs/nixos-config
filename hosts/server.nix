bf4bd2837e026afeb6e3b81689dd975ca62d1ef1
Kyle Carberry <kyle@carberry.com>
2026-05-24 17:50:30 -0400

    Lock down server TCP ingress

--- hosts/server.nix
+++ hosts/server.nix
@@ -1,6 +1,8 @@
 { config, lib, pkgs, ... }:
 
 let
+  publicInterface = "eno1";
+
   sshKeys = [
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCsaay9uprFzsnYG308Ule9vLDdKRJ1cf15ac5SBw3aeRp1bBsEbQaybyTGkFIA2JC2Na1EJS5gi+Y7LOdVGpLJ+JeMrhvVM+wu2tZoS7LdnzhZgtYSNCoCcJsr1P5gSx/1ydJzxUHu/t8DPmN7gUmF4pslAvBZAhwFFlcncgMgwXnLhSl6L4EQrULM+dNYeqRkNg/pl5ilh3rgSpswOd3Dn11EI4dTBwittFBXtV6XvXScK24BRGQKyEun9bun8hANCbgxKTZ/WAXSSChfEGrBBVgY86IoKZiwC7WaiMvd8OtNS6iNnlN0TJOZyyYF1a+m4rJgcLT/M+Q+orBoVYPzPBqY46E5ZGMKNNo3PovdTeT9/uD/NXlQziuwdxURfXWX/7ZSidVpYPHtVdukd9qYUWUyQDE97CNk88JpvBBWQfee3m/2OOOm5yjzwtF27Th65N7NtKMtDNitzg2vurEqbxLTWR69TckOjhOKswA76vps8TAQjigpUSkHBDj8xds= kyle@desktop"
   ];
@@ -108,6 +110,22 @@ in
   networking.firewall = {
     enable = true;
     allowedTCPPorts = [ 22 ];
+    extraCommands = ''
+      iptables -w -I nixos-fw 1 -i ${publicInterface} -p tcp -m conntrack --ctstate NEW -m tcp ! --dport 22 -j DROP
+      ip6tables -w -I nixos-fw 1 -i ${publicInterface} -p tcp -m conntrack --ctstate NEW -m tcp ! --dport 22 -j DROP
+
+      iptables -w -N DOCKER-USER 2>/dev/null || true
+      iptables -w -C FORWARD -j DOCKER-USER 2>/dev/null || iptables -w -I FORWARD 1 -j DOCKER-USER
+      iptables -w -C DOCKER-USER -i ${publicInterface} -p tcp -m conntrack --ctstate NEW -j DROP 2>/dev/null || iptables -w -I DOCKER-USER 1 -i ${publicInterface} -p tcp -m conntrack --ctstate NEW -j DROP
+
+      ip6tables -w -N DOCKER-USER 2>/dev/null || true
+      ip6tables -w -C FORWARD -j DOCKER-USER 2>/dev/null || ip6tables -w -I FORWARD 1 -j DOCKER-USER
+      ip6tables -w -C DOCKER-USER -i ${publicInterface} -p tcp -m conntrack --ctstate NEW -j DROP 2>/dev/null || ip6tables -w -I DOCKER-USER 1 -i ${publicInterface} -p tcp -m conntrack --ctstate NEW -j DROP
+    '';
+    extraStopCommands = ''
+      iptables -w -D nixos-fw -i ${publicInterface} -p tcp -m conntrack --ctstate NEW -m tcp ! --dport 22 -j DROP 2>/dev/null || true
+      ip6tables -w -D nixos-fw -i ${publicInterface} -p tcp -m conntrack --ctstate NEW -m tcp ! --dport 22 -j DROP 2>/dev/null || true
+    '';
   };
 
   environment.systemPackages = with pkgs; [
